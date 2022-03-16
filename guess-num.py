87cd68b130fb80ca0f31f76d277673a850c3f631
Andy Huang <andy2244759@gmail.com>
2022-03-16 23:44:26 +0800

    final version

--- guess-num.py
+++ guess-num.py
@@ -1,8 +1,12 @@
 import random
-awnser = random.randint(1,100)
+L = input("請輸入數字下限: ")
+L = int(L)
+H = input("請輸入數字上限: ")
+H = int(H)
+awnser = random.randint(L,H)
 count = 0
 while True:
-    guess = input("請猜一個1~100的整數: ")
+    guess = input("請猜一個你輸入範圍內的整數: ")
     guess = int(guess)
     count += 1
     if guess == awnser:
